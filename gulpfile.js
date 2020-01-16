57b6389d6f97779541a250257da8f018b0631166
Josh Vickerson <josh@thankview.com>
2020-01-16 11:01:40 -0500

    feat: include built output

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var nano = require("gulp-cssnano");
+var rename = require("gulp-rename");
 
-function build() {
+function compile() {
     return (
         gulp.src('src/utilitarian.scss')
             .pipe(sass())
             .on("error", sass.logError)
-            .pipe(gulp.dest('./build/'))
+            .pipe(gulp.dest('./dist/'))
     )
 }
-exports.build = build;
+
+function minify() {
+    return (
+        gulp.src('dist/utilitarian.css')
+            .pipe(nano())
+            .pipe(rename('utilitarian.min.css'))
+            .pipe(gulp.dest('./dist/'))
+    )
+}
+
+exports.build = gulp.series(compile, minify);
